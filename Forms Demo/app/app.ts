41bc0780a45181ef67cd3c0b9bc5413ccca8c9fc
anashussain08 <anas_hussain08@yahoo.com>
2016-01-09 20:22:12 -0800

    folder structure changed

--- Forms Demo/app/app.ts
+++ Forms Demo/app/app.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core'
-import {HeroFormComponent} from './hero-form.component'
+import {HeroFormComponent} from './components/heroComponent/hero-form.component'
 @Component({
   selector: 'my-form',
   template: '<hero-form></hero-form>',
